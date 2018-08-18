81dc40e0d32891592aa50e5089a9a0bd16eaea02
ncer <huangzzk@bupt.edu.cn>
2018-08-18 08:57:11 +0800

    update

--- cross.py
+++ cross.py
@@ -1,10 +1,10 @@
 from opencv_hcm import *
 import cv2
 import numpy as np
-from m_serial import *
+#from m_serial import *
 import time
 
-
+'''
 cap = cv2.VideoCapture(0)
 
 serial=MySerial(115200)
@@ -14,7 +14,12 @@ while(1):
         (x,y,angle)=get_cross2(frame,getImg=False)
         serial.send_cross_data(x,y)
         time.sleep(0.05)
-
+'''
+img47=cv2.imread("cross47.jpg")
+img55=cv2.imread("cross55.jpg")
+cv2.imshow("47",get_cross2(img47,True))
+cv2.imshow("55",get_cross2(img55,True))
+cv2.waitKey()
 '''
 def get_point(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
--- opencv_hcm.py
+++ opencv_hcm.py
@@ -99,24 +99,23 @@ class Cross:
                 self.cols.append(line)
 
     def get_cross_point(self):
-        '''
-        sumy=0
-        if len(self.rows)!=2 or len(self.cols)!=2:
-            raise Exception
-
-        for i in self.rows:
-            liney=(i.start[1]+i.end[1])/2
-            sumy+=liney
-        cross_y=int(sumy/len(self.rows))
-
-        sumx=0
-        for i in self.cols:
-            linex=(i.start[0]+i.end[0])/2
-            sumx+=linex
-        cross_x=int(sumx/len(self.cols))
-
-        return (cross_x,cross_y)
-        '''
+        if len(self.rows)+len(self.cols)==3:
+            if len(self.rows)==1:
+                width=abs(self.cols[0].start[0]-self.cols[1].start[0])
+                width=int(width)
+                new_row=Line(x1=self.rows[0].start[0],
+                             y1=self.rows[0].start[1]+width,
+                             x2=self.rows[0].end[0],
+                             y2=self.rows[0].end[1]+width)
+                self.add_line(new_row)
+            else:
+                width=abs(self.rows[0].start[1]-self.rows[1].start[1])
+                width=int(width)
+                new_col=Line(x1=self.cols[0].start[0]-width,
+                             y1=self.cols[0].start[1],
+                             x2=self.cols[0].end[0]-width,
+                             y2=self.cols[0].end[1])
+                self.add_line(new_col)
         if len(self.rows)!=2 or len(self.cols)!=2:
             raise Exception
         temp_k=(self.rows[0].k+self.rows[1].k)/2
@@ -207,19 +206,20 @@ def get_cross(image,getImg=True):
         return (x,y)
 
 
-SRC=np.float32([[31,92],[27,327],[395,42],[702,96]])
+SRC=np.float32([[107,210],[415,371],[355,187],[635,236]])
 H=get_H(SRC)
 
 
-def get_cross2(image,getImg=True):
-    image=cv2.warpPerspective(image, H, (0, 0))
-    height=int(image.shape[0])
-    width=int(image.shape[0])
 
+
+def get_cross2(image,getImg=True):
+
+    height=int(image.shape[0])
+    width=int(image.shape[1])
 
     #image=image[0:int(height/12)*6,:]  # 切片，不要的扔掉
 
-    image=image[0:int(height/12)*6:,int(width/4):width]
+    #image=image[0:int(height/12)*6:,int(width/4):width]
 
     gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
@@ -238,6 +238,7 @@ def get_cross2(image,getImg=True):
     #gray = cv2.dilate(gray, kernel, iterations=1)
 
     cimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+
     gray = cv2.dilate(gray, kernel, iterations=1)
     cross=Cross()
     lines=cv2.HoughLinesP(gray,1,np.pi/180,100,minLineLength=100,maxLineGap=300)
@@ -270,23 +271,171 @@ def get_cross2(image,getImg=True):
     else:
         return (x,y,angle)
 
+def get_cross3(image, getImg=True):
+    # 想通过找到逆透视的偏移，来修正地不平，废弃
+    height=image.shape[0]
+    width=image.shape[1]
+
+    image=image[int(height/3):height,:]
+    gray=image
+
+    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # ret, gray = cv2.threshold(gray, 130, 255, 1)
+    gray = cv2.Canny(gray, 200, 100)
+    cv2.imshow("ca2n",gray)
+    #cv2.imshow("can",gray)
+    #cv2.waitKey()
+    #cimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    cimg=image
+    cross = Cross()
+    lines = cv2.HoughLinesP(gray, 1, np.pi / 180, 100, minLineLength=300, maxLineGap=300)
+    if lines is None:
+        if getImg == True:
+            return cimg
+        else:
+            return None
+    my_lines = []
+    for j in lines:
+        i = j[0]
+        x1 = i[0]
+        y1 = i[1]
+        x2 = i[2]
+        y2 = i[3]
+        temp = Line(x1, y1, x2, y2)
+        my_lines.append(temp)
+        cross.add_line(temp)
+        cv2.line(cimg, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    cv2.imshow('test2', cimg)
+    point = cross.get_2lines_cross(my_lines[0], my_lines[1])
+    new_src = SRC + point - SRC[0]
+    new_src = np.float32(new_src)
+    new_H = get_H(new_src)
+
+    gray = cv2.warpPerspective(gray, new_H, (0, 0))
+
+    gray=gray[:,int(width/4):width]
+
+    lines = cv2.HoughLinesP(gray, 1, np.pi / 180, 300, minLineLength=300, maxLineGap=300)
+    cimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    cross = Cross()
+    if lines is None:
+        if getImg == True:
+            return cimg
+        else:
+            return None
+    for j in lines:
+        i = j[0]
+        x1 = i[0]
+        y1 = i[1]
+        x2 = i[2]
+        y2 = i[3]
+        temp = Line(x1, y1, x2, y2)
+        cross.add_line(temp)
+        cv2.line(cimg, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    try:
+        (x,y,angle)=cross.get_cross_point()  # 如果没找够4条线，这里会抛出异常
+        cv2.circle(cimg,(x,y),2,(255,255,0),2)
+        cv2.imshow('test', cimg)
+    except:
+        if getImg==True:
+            return cimg
+        else:
+            return None
+    if getImg==True:
+        print(x,y,angle)
+        return cimg
+    else:
+        return (x,y,angle)
+
+def get_cross4(image,getImg=True):
+    #想通过找数字的中心，来修正偏差
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.Canny(gray, 200, 200)
+    ret, gray = cv2.threshold(gray, 200, 255, 1)
+    cv2.imshow("shold",gray)
+    i,cs,h=cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    for c in cs:
+        x,y,w,h=cv2.boundingRect(c)
+        area=cv2.contourArea(c,True)
+        print(area)
+        if cv2.contourArea(c,True) <4000:
+            continue
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        rect=cv2.minAreaRect(c)
+        box=cv2.boxPoints(rect)
+        box=np.int0(box)
+        cv2.drawContours(image,[box],0,[0,0,255],3)
+    cv2.imshow("can",image)
+
+def get_cross5(image,getImg=True):
+    height=int(image.shape[0])
+    width=int(image.shape[1])
+
+    #image=image[0:int(height/12)*6,:]  # 切片，不要的扔掉
+    image = cv2.warpPerspective(image, H, (0, 0))
+    #image=image[0:int(height/12)*6:,int(width/4):width]
+    #image=image[int(height/12)*:height,:]
+    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    t,contours,h=cv2.findContours(gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)  # 找外轮廓
+    #gray = cv2.Laplacian(gray, cv2.CV_8U, gray,5 )
+    #ret, gray = cv2.threshold(gray, 140, 255, 0)
+    gray=cv2.Canny(gray,300,200)
+    cv2.drawContours(gray,contours,-1,(0,0,0),2)  #去掉外轮廓 实际上是为了去掉透视后的黑边
+    kernel = np.ones((5, 5), np.uint8)
+    #gray = cv2.dilate(gray, kernel, iterations=1)
+
+    lines = cv2.HoughLinesP(gray, 1, np.pi / 180, 100, minLineLength=200, maxLineGap=300)
+    cimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    cross = Cross()
+    if lines is None:
+        if getImg == True:
+            return cimg
+        else:
+            return None
+    for j in lines:
+        i = j[0]
+        x1 = i[0]
+        y1 = i[1]
+        x2 = i[2]
+        y2 = i[3]
+        temp = Line(x1, y1, x2, y2)
+        if abs(temp.k)<1:
+            if y1>300:
+                cross.add_line(temp)
+        else:
+            cross.add_line(temp)
+        cv2.line(cimg, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    try:
+        (x, y, angle) = cross.get_cross_point()  # 如果没找够4条线，这里会抛出异常
+        cv2.circle(cimg, (x, y), 2, (255, 255, 0), 2)
+        cv2.imshow('test', cimg)
+    except:
+        if getImg == True:
+            return cimg
+        else:
+            return None
+    if getImg == True:
+        print(x, y, angle)
+        return cimg
+    else:
+        return (x, y, angle)
+    cv2.imshow("sss",cimg)
 
 
-'''
 #src=np.float32([[116,113],[346,267],[277,84],[601,132]])
 #dst=np.float32([[116,84],[116,267],[277,84],[277,267]])
 #cv2.imshow("test",image)
-src=np.float32([[31,92],[27,327],[395,42],[702,96]])
-H=get_H(src)
+#src=np.float32([[31,92],[27,327],[395,42],[702,96]])
+#H#=get_H(src)
 
-image=cv2.imread("1534521974.jpg")
-image=cv2.warpPerspective(image,H,(0,0))
+image=cv2.imread("1534553104.jpg")
+#image=cv2.warpPerspective(image,H,(0,0))
 
-cross=get_cross2(image,True)
+cross=get_cross5(image,True)
 if cross is not None:
     cv2.imshow("test1",cross)
 
 #cv2.setMouseCallback('test',get_point)
 cv2.waitKey()
 
-'''
